5f2798319502c2ec61fe0a6278a092a79ece572f
Jey Pérez <107527173+Jeimys@users.noreply.github.com>
2024-03-04 22:22:20 -0500

    .

---
+++ bd-imagenes-normalizadas.py
@@ -0,0 +1,30 @@
+import numpy as np
+import tifffile
+import os
+
+# Ruta de las imágenes
+ruta_imagenes = "D:\BDnormalizada"
+
+# Umbral para determinar si una imagen es "enferma"
+umbral = 0.5  
+
+# Recorrer todas las imágenes en la carpeta
+for imagen_nombre in os.listdir(ruta_imagenes):
+    # Leer la imagen
+    imagen_tif = tifffile.imread(os.path.join(ruta_imagenes, imagen_nombre))
+
+    # Normalizar la imagen
+    imagen_normalizada = imagen_tif.astype(np.float32) / np.iinfo(imagen_tif.dtype).max
+        
+    # Determinar la etiqueta según la imagen normalizada
+    if np.any(imagen_normalizada > umbral):
+        print('-'*50)
+        print(f'{imagen_nombre}: Cultivo enfermo')
+        print(np.max(imagen_normalizada))
+        print('-'*50)
+    else:
+        print('-'*50)
+        print(f'{imagen_nombre}: Cultivo sano')
+        print(np.max(imagen_normalizada))
+        print('-'*50)
+        
