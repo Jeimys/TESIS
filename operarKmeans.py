6167068a7a9426e20accbc1af9c80d950f20451e
Jey Pérez <107527173+Jeimys@users.noreply.github.com>
2024-03-26 09:05:15 -0500

    .

--- operarKmeans.py
+++ operarKmeans.py
@@ -8,6 +8,7 @@ Procesador= Algorithms()
 Kmeans=Algorithms()
 SaveDir= "D:\BD"
 ImagePath="D:\IMAGENES\Imagenes\JPG\imagen1.jpg"
+
 #SaveDir= 'C:/Users/Asus/Documents/CRIEE-MAIZ_2023/BD_y_ALGORITMOS/ALGORITMOS/Procesamiento_Imagenes'
 #DirPath= 'C:/Users/Asus/Documents/CRIEE-MAIZ_2023/BD_y_ALGORITMOS/ALGORITMOS/Procesamiento_Imagenes'
 SaveName= 'prueba.png'
